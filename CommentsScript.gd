97886d765cdb43526e137cdfb3007c8047f297e2
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-04-08 21:19:28 -1000

    Add files via upload

---
+++ CommentsScript.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# This is a single line comment
+"""
+This is a 
+multi line comment.
+Keep in mind the compiler sees this as a string.
+"""
+
+var a # everything after the '#' symbol is registered as a comment
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
